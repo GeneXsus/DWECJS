17002659a5e7f2f864435210a947ce5ec5529d8e
Jesus carrillo castillo <carrillocastillojesuscc@gmail.com>
2019-11-08 13:39:41 +0100

    'cookies'

--- Unidad5/ejemplos/cookies/script.js
+++ Unidad5/ejemplos/cookies/script.js
@@ -1,6 +1,9 @@
 "use strict"
 //declararcio variables
-let iClave, iValor,listado;
+let iClave, iValor;
+let mostrado= document.getElementById('mostrado');
+let listado=document.getElementById('listado');
+let arrayCookies;
 //asiganar eventos
 window.addEventListener('load',function(){
     //asignar los objetos a variables
@@ -15,45 +18,66 @@ window.addEventListener('load',function(){
 
 
 function anadirC(){
-    //elimine a los 20 segundos
-    let fecha = new Date();
-    // fecha.setTime(fecha.getTime()+20000);
-    document.cookie= `${iClave.value}=${iValor.value};`;
-    //limpiar datos
-    // iClave.value
+    if (iClave.value!=''){
+        document.cookie= `${iClave.value}=${iValor.value};`;
+
+    }
+
 }
 
 function mostrarC(){
-    let arrayCookies=cargarListaCookie();
-    if (arrayCookies[iClave.value]){
-        document.getElementById('mostrado').innerHTML= `El valor de ${iClave.value}  es ${arrayCookies[iClave.value]}`;
+    listado.innerHTML='';
+    if(existeCookie()){
+            mostrado.innerHTML= `El valor de ${iClave.value}  es ${arrayCookies[iClave.value]}`;
     }else{
-        document.getElementById('mostrado').innerHTML= `No existe la cookie ${iClave.value}`
+        mostrado.innerHTML= `No existe la cookie ${iClave.value}`
     }
 }
 
 function listarC(){
-    let arrayCookies=cargarListaCookie();
-
-    for (let key in arrayCookies)
-    {
-        document.getElementById('listado').innerHTML+= `La clave es ${key} y su valor es ${arrayCookies[key]} <br>`
-        
-    };
-
+  
+    listado.innerHTML='';
+    mostrado.innerHTML='';
+    if(document.cookie != ''){
+        arrayCookies=cargarListaCookie();
+        for (let key in arrayCookies)
+        {
+            listado.innerHTML+= `La clave es ${key} y su valor es ${arrayCookies[key]} <br>`
+        };
+    }else{
+        mostrado.innerHTML= 'No hay cookies';
+    }
 }
 
 function eliminarC(){
     let fecha = new Date();
+    listado.innerHTML='';
     fecha.setTime(fecha.getTime()-20000);
-    document.cookie=`${iClave.value}=${iValor.value}; expires=${fecha.toGMTString()};`;
-    document.getElementById('mostrado').innerHTML= 'Cookie eliminada';
+    if(existeCookie()){
+            document.cookie=`${iClave.value}=; expires=${fecha.toGMTString()};`;
+            document.getElementById('mostrado').innerHTML= 'Cookie eliminada';
+    }else{
+        mostrado.innerHTML= `No existe la cookie ${iClave.value}`
+    }
 }
 
+function existeCookie(){
+    if(document.cookie != ''){
+        arrayCookies= cargarListaCookie();
+        if (arrayCookies[iClave.value]){
+            return true 
+        }
+    }
+    return false;
+
+}
 
 function cargarListaCookie(){
     return document.cookie.split(';').reduce((cookies, cookie) => {
-        let [ name, value ] = cookie.split('=').map(c => c.trim());
+        // let [ name, value ] = cookie.split('=').map(c => c.trim()); Esto sería con mapa
+        let cookieSplit= cookie.split('=');
+        let name= cookieSplit[0].trim();
+        let value= cookieSplit[1].trim();
         cookies[name] = value;
         return cookies;
       }, {});
